dd21c50fa61b91c3d069a6f6b53aeb3c7cec76bb
Daniel Lima De Farias <106928720+Danieldevjr@users.noreply.github.com>
2024-04-21 19:31:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,18 @@
+function validateEmail() {
+    const email = document.getElementById('email').value;
+    const emailError = document.getElementById('email-error');
 
-function validarEmail(email) {
-    // Expressão regular para validar emails
-    var re = /^(([^<>()\[\]\\.,;:\s@\"]+\.[^<>()\[\]\\.,;:\s@\"]+)|((\w+\.[^\w\-]+)@(([\w\-.]+\.)+[a-zA-Z]{2,})))$/;
-    return re.test(email);
-}
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      // Email válido!
+      emailError.textContent = '';
 
-function validarFormulario() {
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-    var assunto = document.getElementById('assunto').value;
-    var mensagem = document.getElementById('mensagem').value;
+      // Exibir alerta de email válido (opcional):
+      alert('Email válido!');
 
-    // Validação do campo de nome
-    if (nome === "") {
-        alert('Por favor, preencha o campo "Nome".');
+      return true;
+    } else {
+        // Email inválido
+        emailError.textContent = 'Por favor, digite um email válido (exemplo: email@provedor.com)';
         return false;
+      }
     }
-
-    // Validação do campo de email
-    if (!validarEmail(email)) {
-        alert('Por favor, digite um email válido.');
-        return false;
-    }
-
-    // Validação do campo de assunto
-    if (assunto === "") {
-        alert('Por favor, preencha o campo "Assunto".');
-        return false;
-    }
-
-    // Validação do campo de mensagem
-    if (mensagem === "") {
-        alert('Por favor, preencha o campo "Mensagem".');
-        return false;
-    }
-
-    // Se todos os campos estiverem válidos, envie o formulário
-    return true;
-}
-
-// Adicione o evento "onsubmit" ao formulário para chamar a função validarFormulario()
-document.getElementById('contatoForm').onsubmit = function () {
-    return validarFormulario();
-};
